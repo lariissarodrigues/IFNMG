fb69b7ed2e903e3d99dbbb2256ee0dc318b4ecd4
Larissa Rodrigues <104832983+lariissarodrigues@users.noreply.github.com>
2022-08-19 13:29:41 -0300

    Delete Des.Sistemas/Listas/Lista04 directory

--- Des.Sistemas/Listas/Lista04/Questao03/src/Principal.java
+++
@@ -1,9 +0,0 @@
-public class Principal {
-    public static void main(String[] args) {
-        Carro c1 = new Carro(400, 10);
-
-        c1.isLigado(false);
-        c1.acelerar();
-        System.out.println(c1.getvelocidade());
-    }
-}
